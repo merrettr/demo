d2ebd2e300626cb2fa25d470aa92cdba5f618438
Reece Merrett <reecemerrett@gmail.com>
2017-04-02 18:51:21 +1200

    Make User component more responsive

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Panel from 'react-bootstrap/lib/Panel';
 import Col from 'react-bootstrap/lib/Col';
+import Grid from 'react-bootstrap/lib/Grid';
 
 export default ({
   id,
@@ -10,51 +11,61 @@ export default ({
   website,
   company,
 }) =>
-  <div
+  <Grid
+    fluid
     style={{
       display: 'flex',
       justifyContent: 'center',
-      marginTop: '10em',
     }}
   >
-    <Panel
-      style={{
-        maxWidth: '50%',
-        flex: 1,
-        wordWrap: 'break-word',
-        paddingTop: '3em',
-      }}
+    <Col
+      xs={12}
+      md={8}
+      style={{ marginTop: '10em' }}
     >
-      <Col
-        xs={12}
-        md={8}
+      <Panel
         style={{
-          textAlign: 'center',
-          fontSize: '1.8em',
+          wordWrap: 'break-word',
+          paddingTop: '3em',
         }}
       >
-        {name}
-      </Col>
-      <Col
-        xs={12}
-        md={4}
-        style={{ flex: 1 }}>
-        <div>
-          <i>{company.name}</i>
+        <Col
+          xs={12}
+          md={8}
+          style={{
+            textAlign: 'center',
+            fontSize: '1.8em',
+            paddingBottom: '2em',
+          }}
+        >
+          {name}
+        </Col>
+        <Col
+          xs={12}
+          md={4}
+          style={{
+            flex: 1,
+            borderLeft: 'thin #000',
+            borderLeftStyle: 'solid',
+          }}
+        >
+          <div>
+            <i>{company.name}</i>
+            <br />
+            <i>{company.catchPhrase}</i>
+            <br />
+            <i>{company.bs}</i>
+            <br />
+          </div>
           <br />
-          <i>{company.catchPhrase}</i>
-          <br />
-          <i>{company.bs}</i>
-          <br />
-        </div>
-        <br />
-        <div>
-          <b>Phone:</b> <i>{phone}</i>
-          <br />
-          <b>Email:</b> <i>{email}</i>
-          <br />
-          <b>Website:</b> <i>{website}</i>
-        </div>
-      </Col>
-    </Panel>
-  </div>;
+          <div>
+            <b>Phone:</b> <i>{phone}</i>
+            <br />
+            <b>Email:</b> <i>{email}</i>
+            <br />
+            <b>Website:</b> <i>{website}</i>
+          </div>
+        </Col>
+      </Panel>
+    </Col>
+  </Grid>;
